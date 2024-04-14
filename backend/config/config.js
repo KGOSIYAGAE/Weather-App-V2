5f75ad505f170959af97a8eb28e53a45783a93ef
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-15 00:45:11 +0200

    Sever request route completed

---
+++ backend/config/config.js
@@ -0,0 +1,6 @@
+export const PORT = 3000;
+export const APIKey = "69b03d0815bd8cf2798a9a453358a2b6";
+//export cont URI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}"
+export const URI = "http://api.openweathermap.org/data/2.5/forecast?";
+//export const GEO_URI = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}"
+export const GEO_URI = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,43 @@
 import express from "express";
+import { PORT } from "./config/config.js";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const GEO_URL = "http://api.openweathermap.org/";
+const location = "London";
+const APIKey = "69b03d0815bd8cf2798a9a453358a2b6";
+const limit = 1;
 
-app.use(port, () => {
-  console.log(`Server running on port${port}`);
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Method Get coordinates of location
+const getCoordinates = async () => {
+  try {
+    const response = await axios.get(GEO_URL + `geo/1.0/direct?q=${location}&limit=${limit}&appid=${APIKey}`);
+    const results = { data: response.data[0] };
+    return results.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Get coordinates of location
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = await getCoordinates();
+
+  try {
+    const response = await axios.get(WEATHER_URL + `lat=${lat}&lon=${lon}&appid=${APIKey}`);
+    res.status(200).json({ data: response.data });
+  } catch (error) {
+    console.log(error.message);
+    res.status(501).send({ message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port${PORT}`);
 });
