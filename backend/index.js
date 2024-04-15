a513b6cf6caa5ed00fbf38bc983c7f4c626041f8
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-15 21:08:57 +0200

    Updated to new API

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const location = "London";
 const APIKey = "69b03d0815bd8cf2798a9a453358a2b6";
 const limit = 1;
 
-const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+const WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall?";
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 //Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,9 +29,9 @@ const getCoordinates = async () => {
 //Get coordinates of location
 app.get("/weather", async (req, res) => {
   const { lat, lon } = await getCoordinates();
-
+  //&exclude=minutely,hourly,alert&appid=${APIKey}`
   try {
-    const response = await axios.get(WEATHER_URL + `lat=${lat}&lon=${lon}&appid=${APIKey}`);
+    const response = await axios.get(WEATHER_URL + `lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}`);
     res.status(200).json({ data: response.data });
   } catch (error) {
     console.log(error.message);
